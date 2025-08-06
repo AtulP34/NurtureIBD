839668252386b56b7af0782d5750ef6f8fdf2b50
AtulP34 <atul.pillai23@gmail.com>
2025-08-06 10:01:05 -0700

    Update App.py

--- App.py
+++ App.py
@@ -6,9 +6,9 @@ import google.generativeai as genai
 import pandas as pd
 
 # Setup Tesseract OCR
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 # Gemini API Setup
-genai.configure(api_key="st.secrets["apikey"]")
+genai.configure(api_key=st.secrets["apikey"])
 model = genai.GenerativeModel("gemini-2.5-flash")
 # Sidebar
 st.sidebar.title(":clipboard: Menu")
